59647b8ae8ba25893a1d8774873b5dcb3f423389
zhaoyuxiang <zhaoyuxiang@yidian-inc.com>
2018-04-21 17:08:54 +0800

    add log rotate

--- log.go
+++ log.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"time"
 )
 
 const (
@@ -22,34 +23,98 @@ type Logger struct {
 var Log Logger
 var f *os.File
 
+var date = ""
+var dateFormat = "2006-01-02"
+
 func main() {
-	Log.Error("000")
-	Log.Warn("111")
-	Log.Info("222")
-	Log.Debug("333")
-	Close()
+
+	i := 1
+	for {
+		Log.Error("000")
+		Log.Warn("111")
+		Log.Info("222")
+		Log.Debug("333")
+		i++
+		if i > 10000000 {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	Destroy()
 }
 
 func init() {
-	path := "/var/logs/error.log"
+	initLogger()
+}
 
+func initLogger() {
+	checkRotate()
+
+	Log.level = 2
+	Log.callDepth = 2
+}
+
+func checkRotate() {
+	nowDate := getFormatDate()
+	if nowDate != date {
+
+		if f != nil {
+			rotateLog()
+		}
+
+		path := getLogPath()
+		date = nowDate
+
+		openFile(path)
+
+		Log.l = log.New(f, "", log.Lshortfile|log.Ldate|log.Lmicroseconds)
+	}
+}
+
+func Destroy() {
+	closeFile()
+}
+
+func openFile(path string) {
 	var err error
 
 	f, err = os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	Log.l = log.New(f, "", log.Lshortfile|log.Lmicroseconds)
+func closeFile() {
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
 
-	Log.level = 2
-	Log.callDepth = 2
+func getLogPath() string {
+	path := "/var/logs/error.log"
+	return path
+}
+
+func rotateLog() {
+	closeFile()
+	oldName := getLogPath()
+	newName := oldName + time.Now().Add(-time.Second * 86400).Format(dateFormat)
+	err := os.Rename(oldName, newName)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getFormatDate() string {
+	return time.Now().Format(dateFormat)
 }
 
 func (ll Logger) Error(v interface{}) {
 	if ll.level < LevelError {
 		return
 	}
+	checkRotate()
 
 	ll.l.SetPrefix("[Error] : ")
 	ll.l.Output(ll.callDepth, fmt.Sprintln(v))
@@ -59,6 +124,7 @@ func (ll Logger) Warn(v interface{}) {
 	if ll.level < LevelWarn {
 		return
 	}
+	checkRotate()
 
 	ll.l.SetPrefix("[Warn] : ")
 	ll.l.Output(ll.callDepth, fmt.Sprintln(v))
@@ -68,6 +134,7 @@ func (ll Logger) Info(v interface{}) {
 	if ll.level < LevelInfo {
 		return
 	}
+	checkRotate()
 
 	ll.l.SetPrefix("[Info] : ")
 	ll.l.Output(ll.callDepth, fmt.Sprintln(v))
@@ -77,13 +144,8 @@ func (ll Logger) Debug(v interface{}) {
 	if ll.level < LevelDebug {
 		return
 	}
+	checkRotate()
 
 	ll.l.SetPrefix("[Debug] : ")
 	ll.l.Output(ll.callDepth, fmt.Sprintln(v))
-}
-
-func Close() {
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
 }
